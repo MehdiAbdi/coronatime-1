86ca6934376f864b14becda30da53e753a84ddb7
Mehdi <appleseed.me77@gmail.com>
2021-01-03 10:48:07 +0330

    Initial Commit

---
+++ CoronaTime-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoronaTime-1
+//
+//  Created by Mehdi Abdi on 1/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
